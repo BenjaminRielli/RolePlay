2fb32a188b446fd5776d5c27b0f9416670a6f13b
BenjaminRielli <101676520+BenjaminRielli@users.noreply.github.com>
2022-04-30 19:02:03 -0300

    Clase Enano

--- src/Library/Enano.cs
+++ src/Library/Enano.cs
@@ -1,7 +1,38 @@
+using System;
 namespace Roleplay
 {
     public class Enano
     {
-        
+        public Enano(string nombre, int vida, int ataque, int defensa)
+        {
+            //List<Elemento> ElementosAdquiridos = new List<Elemento>();
+
+            this.Nombre = nombre;
+            this.Vida = vida;
+            this.Ataque = ataque;
+            this.Defensa = defensa;
+        }
+        //private List<Elemento> ElementosAdquiridos {get; set; } 
+        public string Nombre { get; }
+        public int Vida { get; set; }
+        public int Ataque { get; }
+        public int Defensa { get; }
+
+        /* public void Equipar(Elemento elemento){
+            // agregar elemento a lista de elementos
+            ElementosAdquiridos.Add(elemento);
+            
+        }
+        public void Desequipar(Elemento elemento){
+            // desequipar elemento de lista
+            ElementosAdquiridos.Remove(elemento);
+        }
+        public void Atacar(){
+            // atacar           
+        }
+        public void Hechizar(){
+            // hechizar
+        } */
     }
+
 }
