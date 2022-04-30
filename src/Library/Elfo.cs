227c003cf1fd634d418a97830b4f064f9e0f1b64
NicolasAbreuUCU <101676516+NicolasAbreuUCU@users.noreply.github.com>
2022-04-30 17:18:42 -0300

    Update Elfo.cs
    Agregando funcionabilidades a la clase Elfo.

--- src/Library/Elfo.cs
+++ src/Library/Elfo.cs
@@ -2,6 +2,23 @@ namespace Roleplay
 {
     public class Elfo
     {
-        
+        public Elfo(string name, int life, int attack, int defensa){
+            this.Name = name;
+            this.Life = life;
+            this.Attack = attack;
+            this.Defensa = defensa;
+        }
+        public string Name{ get; }
+        public int Life{ get; private set; }
+        public int Attack{ get; }
+        public int Defensa{ get; }
+        public void AttackEnemy(int n)
+        {
+            this.Life = Life - n;
+        }
+        public void Curar(int n)
+        {
+            this.Life = Life + n;
+        }        
     }
 }
